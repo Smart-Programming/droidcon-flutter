fca6f453d8be8fedebd1bd6f7020dd22ba6f0fd7
Nderi12 <nderikamau1212@gmail.com>
2023-11-09 08:00:28 +0300

    New UI changes

--- lib/src/presentation/router/route_generator.dart
+++ lib/src/presentation/router/route_generator.dart
@@ -1,3 +1,5 @@
+import 'package:droidcon_flutter/src/presentation/pages/article/list_page.dart';
+import 'package:droidcon_flutter/src/presentation/pages/article/single_topic_page.dart';
 import 'package:droidcon_flutter/src/presentation/pages/dashboard/dashboard_page.dart';
 import 'package:droidcon_flutter/src/presentation/pages/kanban/task_board.dart';
 import 'package:droidcon_flutter/src/presentation/pages/landing_page.dart';
@@ -8,7 +10,7 @@ Route<dynamic> generateRoute(RouteSettings settings) {
   /// TODO: Uncomment the following line if you need
   /// to pass any properties to a page as route arguments
 
-  // final dynamic routeArgs = settings.arguments
+  final dynamic routeArgs = settings.arguments;
 
   switch (settings.name) {
     case AppRoutes.dashboardRoute:
@@ -17,6 +19,14 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       );
     case AppRoutes.kanbanRoute:
       return MaterialPageRoute<TaskBoard>(builder: (_) => TaskBoard());
+    case AppRoutes.articlesRoute:
+      return MaterialPageRoute<TaskBoard>(builder: (_) => ArticlesList());
+    case AppRoutes.singleArticleRoute:
+      return MaterialPageRoute<TaskBoard>(
+        builder: (_) => SingleTopicPage(
+          props: routeArgs as Props,
+        ),
+      );
     default:
       return MaterialPageRoute<LandingPage>(
         builder: (_) => LandingPage(),
